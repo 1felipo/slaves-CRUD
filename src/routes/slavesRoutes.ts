0b00db851d72fae80824e992246a20b91f3be4c0
felipendelicia <delicia4581@gmail.com>
2022-05-29 19:34:15 -0300

    feat: adding more info in the res.send

--- src/routes/slavesRoutes.ts
+++ src/routes/slavesRoutes.ts
@@ -6,20 +6,21 @@ import Slave from  "../models/slaves"
 
 router.get("/", async (req: Request,res: Response)=>{
     const slaves = await Slave.find()
-    console.log(colors.green("The slaves were gived successfully"))
+    console.log(colors.green(`The slaves (lenght: ${slaves.length}) were gived successfully`))
     res.send(slaves)
 }) // Get the slaves
 
 router.post("/add", async (req:Request,res:Response)=>{
     const slave = new Slave(req.body)
     await slave.save()
-    console.log(colors.green("The slave was registered in the db successfully"))
+    console.log(colors.green(`The slave ("${slave.slave}") was registered in the db successfully`))
 }) // Add new slaves
 
 router.delete("/delete/:id", async (req:Request,res:Response)=>{
     const id = req.params.id
+    const slave = await Slave.findById(id)
     await Slave.deleteOne({_id:id})
-    console.log(colors.green("The slave was deleted from the db successfully"))
+    console.log(colors.green(`The slave ("${slave.slave}") was deleted from the db successfully`))
 }) // Delete the slaves
 
 router.put("/turn/:id", async (req:Request, res:Response)=>{
