ca2ca4aafba8057e8cee2a7d78df1fe0745c133e
felipendelicia <delicia4581@gmail.com>
2022-05-30 00:50:44 -0300

    feat: structure and styles slaves-container

--- frontend/src/components/SlavesContainer/SlavesContainer.tsx
+++ frontend/src/components/SlavesContainer/SlavesContainer.tsx
@@ -1,13 +1,15 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
 
 import config from '../../configuration/config'
+import ThemeContext from '../../configuration/theme';
+import "./SlavesContainer.css"
 
 const SlavesContainer = () => {
 
-  const [slaves, setSlaves] = useState([])
+  const [slaves, setSlaves] = useState<{description:string;slave:string;status:boolean;__v:string;_id:string}[]>()
 
-  const getSlaves = async () => {
+  const getSlaves = () => {
     axios.get(config.slavesEndPoint)
     .then(res => {
         setSlaves(res.data);
@@ -18,11 +20,32 @@ const SlavesContainer = () => {
     getSlaves()
   },[])
 
-  console.log(slaves)
+  const theme = useContext(ThemeContext)
 
   return (
-    <div className="slavescontainer-container">
-
+    <div className="slaves-container-container">
+      {
+        slaves?.map((slave,i)=>{
+          console.log(slave.status)
+          return(
+            <div className='slave-container-main' 
+            key={i}
+            style={{background:theme.softColor}}>
+              <div className="slave-data">
+                <h3 style={{color:theme.inverseColor}}>{slave.slave}</h3>
+                <p style={{color:theme.inverseColor}}>{slave.description}</p>
+              </div>
+              <div className="slave-controllers">
+                <button>Delete</button>
+                <button style={slave.status?{background:"#36AE7C"}:{background:"#F32424"}}>
+                  Sold?
+                </button>
+                <button>Edit</button>
+              </div>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
