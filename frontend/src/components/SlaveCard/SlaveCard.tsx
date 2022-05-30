5c31996d1832a84b39d8603508a395884e0dba4d
felipendelicia <delicia4581@gmail.com>
2022-05-30 14:02:16 -0300

    feat: moved the slaves to a slaves card component functional

---
+++ frontend/src/components/SlaveCard/SlaveCard.tsx
@@ -0,0 +1,113 @@
+import { useState, useContext } from "react";
+import axios from "axios";
+
+import ThemeContext from "../../configuration/theme";
+import config from "../../configuration/config";
+import "./SlaveCard.css";
+
+interface SlaveCardProps {
+  slaveId: string;
+  slaveName: string;
+  slaveDescription: string;
+  slaveStatus: boolean;
+}
+
+const SlaveEditCard = (props: SlaveCardProps) => {
+  const [slaveNameEdit, setSlaveNameEdit] = useState(props.slaveName);
+  const [slaveDescriptionEdit, setSlaveDescriptionEdit] = useState(
+    props.slaveDescription
+  );
+
+  const theme = useContext(ThemeContext);
+
+  const putSlaveEdit = (id: string) => {
+    let data = {
+      slave: slaveNameEdit,
+      description: slaveDescriptionEdit,
+    };
+    axios.put(config.putSlaveEditEndPoint + id, data);
+  };
+
+  const handleChangeName = (e: React.FormEvent<HTMLInputElement>) => {
+    setSlaveNameEdit(e.currentTarget.value);
+  };
+  const handleChangeDescription = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    setSlaveDescriptionEdit(e.currentTarget.value);
+  };
+
+  return (
+    <div
+      className="edit-slave-container"
+      style={{ background: theme.softColor }}
+    >
+      <input
+        style={{ background: theme.softColor }}
+        value={slaveNameEdit}
+        onChange={(event) => handleChangeName(event)}
+      />
+      <textarea
+        style={{ background: theme.softColor }}
+        value={slaveDescriptionEdit}
+        onChange={(event) => handleChangeDescription(event)}
+      ></textarea>
+      <button onClick={() => putSlaveEdit(props.slaveId)}>Edit</button>
+    </div>
+  );
+};
+
+const SlaveCard = (props: SlaveCardProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const deleteSlave = async (id: string) => {
+    await axios.delete(config.deleteSlaveEndPoint + id);
+  };
+  const putSlaveStatus = (id: string) => {
+    axios.put(config.putSlaveStatusEndPoint + id);
+  };
+
+  const handleToggleEdit = () => {
+    isEditing ? setIsEditing(false) : setIsEditing(true);
+  };
+
+  const theme = useContext(ThemeContext);
+  return (
+    <>
+      <div
+        className="slave-container-main"
+        style={{ background: theme.softColor }}
+      >
+        <div className="slave-data">
+          <h3 style={{ color: theme.inverseColor }}>{props.slaveName}</h3>
+          <p style={{ color: theme.inverseColor }}>{props.slaveDescription}</p>
+        </div>
+
+        <div className="slave-controllers">
+          <button onClick={() => deleteSlave(props.slaveId)}>Delete</button>
+          <button
+            style={
+              props.slaveStatus
+                ? { background: "#36AE7C" }
+                : { background: "#F32424" }
+            }
+            onClick={() => putSlaveStatus(props.slaveId)}
+          >
+            Sold?
+          </button>
+          <button onClick={handleToggleEdit}>Edit</button>
+        </div>
+      </div>
+      {isEditing ? (
+        <SlaveEditCard
+          slaveId={props.slaveId}
+          slaveName={props.slaveName}
+          slaveDescription={props.slaveDescription}
+          slaveStatus={props.slaveStatus}
+        />
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default SlaveCard;
--- frontend/src/components/SlavesContainer/SlavesContainer.tsx
+++ frontend/src/components/SlavesContainer/SlavesContainer.tsx
@@ -1,15 +1,13 @@
 import axios from 'axios'
-import React, { useEffect, useState, useContext } from 'react'
+import { useEffect, useState } from 'react'
 
 import config from '../../configuration/config'
-import ThemeContext from '../../configuration/theme';
+import SlaveCard from '../SlaveCard/SlaveCard';
 import "./SlavesContainer.css"
 
 const SlavesContainer = () => {
 
   const [slaves, setSlaves] = useState<{description:string;slave:string;status:boolean;__v:string;_id:string}[]>()
-  const [slaveNameEdit, setSlaveNameEdit] = useState("")
-  const [slaveDescriptionEdit, setSlaveDescriptionEdit] = useState("")
 
   const getSlaves = () => {
     axios.get(config.getSlavesEndPoint)
@@ -22,57 +20,17 @@ const SlavesContainer = () => {
     getSlaves()
   },[slaves])
 
-  const deleteSlave = async (id:string) => {
-    await axios.delete(config.deleteSlaveEndPoint+id)
-  }
-
-  const putSlaveStatus = (id:string) => {
-    axios.put(config.putSlaveStatusEndPoint+id)
-  }
-
-  const putSlaveEdit = (id:string) => {
-    alert("En desarrollo")
-  }
-
-  const theme = useContext(ThemeContext)
-
   return (
     <div className="slaves-container-container">
       {
         slaves?.map((slave,i)=>{
           return(
-            <>
-            <div className='slave-container-main' 
-            key={i}
-            style={{background:theme.softColor}}>
-
-              <div className="slave-data">
-                <h3 style={{color:theme.inverseColor}}>{slave.slave}</h3>
-                <p style={{color:theme.inverseColor}}>{slave.description}</p>
-              </div>
-
-              <div className="slave-controllers">
-                <button onClick={()=>deleteSlave(slave._id)}>
-                  Delete
-                </button>
-                <button 
-                style={slave.status?{background:"#36AE7C"}:{background:"#F32424"}}
-                onClick={()=>putSlaveStatus(slave._id)}>
-                  Sold?
-                </button>
-                <button onClick={()=>putSlaveEdit(slave._id)}>
-                  Edit
-                </button>
-              </div>
-            </div>
-            <div className="edit-slave-container" style={{background:theme.softColor}}>
-              <input style={{background:theme.softColor}}
-              value={slave.slave}/>
-              <textarea style={{background:theme.softColor}}
-              value={slave.description}></textarea>
-              <button>Edit</button>
-            </div>
-            </>
+            <SlaveCard 
+            slaveId={slave._id}
+            slaveName={slave.slave}
+            slaveDescription={slave.description}
+            slaveStatus={slave.status}
+            key={i}/>
           )
         })
       }
